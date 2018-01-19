70f0508ff8926e27e60277ba0a7c2d68fe11f80c
learnprogrammingseries <35609848+learnprogrammingseries@users.noreply.github.com>
2018-01-19 17:03:12 -0200

    Added the convert static function

--- converter.js
+++ converter.js
@@ -21,7 +21,42 @@ class Converter {
     static FtoK(f) {
         return 273.15 + (f - 32) * 5 / 9;
     }
-
+	
+	
+    static convert() {
+        var value = document.getElementById("temperature").value;
+        if (value == "") // In case value is empty, the function should finish.
+            return;
+        var from = document.getElementById("fromXdegrees").value;
+        var to = document.getElementById("toYdegrees").value;
+        var result;
+        switch (from + "/" + to) {
+            case "fahrenheit/celsius":
+                result = this.FtoC(value);
+                break;
+            case "celsius/fahrenheit":
+                result = this.CtoF(value);
+                break;
+            case "kelvin/celsius":
+                result = this.KtoC(value);
+                break;
+            case "celsius/kelvin":
+                result = this.CtoK(value);
+                break;
+            case "fahrenheit/kelvin":
+                result = this.FtoK(value);
+                break;
+            case "kelvin/fahrenheit":
+                result = this.KtoF(value);
+                break;
+            default:
+                document.getElementById("result").textContent = "You can't convert to the same temperature scale!";
+                return;
+        }
+        result = value + " " + from + " is equivalent to " + result + " " + to;
+        document.getElementById("result").textContent = result;
+    }
+	
 }
 
 var buttonConvert = document.getElementById("convert");
