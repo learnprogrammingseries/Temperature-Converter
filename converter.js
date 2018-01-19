2457e9459626d1460093b014a2f2df590ece94c6
learnprogrammingseries <35609848+learnprogrammingseries@users.noreply.github.com>
2018-01-19 16:56:29 -0200

    Added the converter functions - all static

--- converter.js
+++ converter.js
@@ -1,6 +1,25 @@
 class Converter {
     constructor() {
-        
+
+    }
+
+    static FtoC(f) {
+        return (f - 32) * 5 / 9;
+    }
+    static CtoF(c) {
+        return c * 9 / 5 + 32;
+    }
+    static KtoC(k) {
+        return k - 273.15;
+    }
+    static CtoK(c) {
+        return Number(c) + 273.15;
+    }
+    static KtoF(k) {
+        return (k - 273.15) * 9 / 5 + 32;
+    }
+    static FtoK(f) {
+        return 273.15 + (f - 32) * 5 / 9;
     }
 
 }
